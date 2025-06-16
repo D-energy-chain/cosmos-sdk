1a535fa4d949ef2dea92ca4b83206a6aec6d6b0c
dev-zaid <zaid.tab123@gmail.com>
2025-06-16 16:37:48 +0530

    fix: NFT to Coin Conversion Calculation

--- x/staking/migrations/v1/types.go
+++ x/staking/migrations/v1/types.go
@@ -82,14 +82,7 @@ func AddressFromLastValidatorPowerKey(key []byte) []byte {
 func GetValidatorsByPowerIndexKey(validator types.Validator) []byte {
 	// NOTE the address doesn't need to be stored because counter bytes must always be different
 	// NOTE the larger values are of higher value
-	tokenValue := validator.Tokens
-
-	if validator.TotalNftDelegation.GT(math.ZeroInt()) {
-		nftToCoinValue := validator.TotalNftDelegation.Quo(math.NewInt(1000))
-		tokenValue = tokenValue.Add(nftToCoinValue)
-	}
-
-	consensusPower := sdk.TokensToConsensusPower(tokenValue, sdk.DefaultPowerReduction)
+	consensusPower := sdk.TokensToConsensusPower(validator.TokensPowerValue(), sdk.DefaultPowerReduction)
 	consensusPowerBytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(consensusPowerBytes, uint64(consensusPower))
 
--- x/staking/types/keys.go
+++ x/staking/types/keys.go
@@ -116,14 +116,8 @@ func GetValidatorsByPowerIndexKey(validator Validator, powerReduction math.Int, 
 	// NOTE the larger values are of higher value
 
 	// This is to calculate the Power of the Validator based on the NFTs
-	tokenValue := validator.Tokens
 
-	if validator.TotalNftDelegation.GT(math.ZeroInt()) {
-		nftToCoinValue := validator.TotalNftDelegation.Quo(math.NewInt(1000))
-		tokenValue = tokenValue.Add(nftToCoinValue)
-	}
-
-	consensusPower := sdk.TokensToConsensusPower(tokenValue, powerReduction)
+	consensusPower := sdk.TokensToConsensusPower(validator.TokensPowerValue(), powerReduction)
 	consensusPowerBytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(consensusPowerBytes, uint64(consensusPower))
 
