fa1ff643e173c41f04b1d62d917ad48867b47d40
dev-zaid <zaid.tab123@gmail.com>
2025-08-26 15:50:12 +0530

    refactor: remove deprecated community tax parameter from legacy distribution settings

--- x/distribution/types/params_legacy.go
+++ x/distribution/types/params_legacy.go
@@ -16,7 +16,6 @@ func ParamKeyTable() paramtypes.KeyTable {
 // Deprecated: ParamSetPairs returns the parameter set pairs.
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 	return paramtypes.ParamSetPairs{
-		paramtypes.NewParamSetPair(ParamStoreKeyCommunityTax, &p.CommunityTax, validateCommunityTax),
 		paramtypes.NewParamSetPair(ParamStoreKeyWithdrawAddrEnabled, &p.WithdrawAddrEnabled, validateWithdrawAddrEnabled),
 	}
 }
